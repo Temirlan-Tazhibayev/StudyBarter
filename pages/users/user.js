e585fdda29fc883b7c041bb2f19e756dc6b241ef
Temirlan-Tazhibayev <79756276+Temirlan-Tazhibayev@users.noreply.github.com>
2024-03-07 01:39:53 +0500

    user page

--- pages/users/user.js
+++ pages/users/user.js
@@ -13,7 +13,23 @@ export default function User() {
     const handleTabChange = (tab) => {
         setActiveTab(tab);
     };
+    const [settingTab, setSettingTab] = useState('PersonalInformation');
+    const handleSettingTabChange = (tab) => {
+        setSettingTab(tab);
+    } 
 
+    const [isConfirmed, setIsConfirmed] = useState(false);
+
+    const handleDeleteProfile = () => {
+        if (isConfirmed) {
+            
+            
+
+            console.log("Deleting profile...");
+        } else {
+            alert("Please confirm that you understand the implications.");
+        }
+    };
 
     return (
         <>
@@ -104,22 +120,80 @@ export default function User() {
                             )}
                             {activeTab === 'Settings' && (
                                 <>
-                                    <nav>
+                                    <nav className={style.SettingsNav}>
                                         <ul>
-                                            <li>PERSONAL INFORMATION</li>
-                                            <li>
-                                                <a className={style.SettingsNavs}>Edit Profile</a>
+                                            <li className={style.SettingsNavElement}>PERSONAL INFORMATION</li>
+                                            <li className={style.SettingsNavElement}>
+                                                <a className={settingTab === 'EditProfile' ? `${style.SettingsNavElementButton} ${style.active}` : style.SettingsNavElementButton} onClick={() => handleSettingTabChange('EditProfile')}>Edit Profile Settings</a>
+                                            
                                             </li>
-                                            <li>
-                                                <a className={style.SettingsNavs}>Edit Email Settings</a>
+                                            <li className={style.SettingsNavElement}>
+                                                <a className={settingTab === 'EditEmail' ? `${style.SettingsNavElementButton} ${style.active}` : style.SettingsNavElementButton} onClick={() => handleSettingTabChange('EditEmail')}>Edit Email Settings</a>
                                             </li>
-                                            <li>
-                                                <a className={style.SettingsNavs}>Delete Profile</a>
+                                            <li className={style.SettingsNavElement}>
+                                                <a className={settingTab === 'DeleteProfile' ? `${style.SettingsNavElementButton} ${style.active}` : style.SettingsNavElementButton} onClick={() => handleSettingTabChange('DeleteProfile')}>Delete Profile</a>
+
                                             </li>
                                         </ul>
                                     </nav>
-                                    <div>
+                                    <div className={style.SettingsBody}>
+                                        {settingTab === 'EditProfile' && (
+                                            <>
+                                                <h2 className={style.sheader}>Edit you profile</h2>
+                                                <div className={style.sbody}>
+                                                    <div className={style.sbodyElement}>
+                                                        Profile image
+                                                    </div>
+                                                    <div className={style.sbodyElement}>
+                                                        Display Name
+                                                        <input></input>
+                                                    </div>
+                                                    <div className={style.sbodyElement}>
+                                                        Title
+                                                        <input></input>
+                                                    </div>
+                                                    <div className={style.sbodyElement}>
+                                                        About Me
+                                                        <input></input>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )}
+                                        {settingTab === 'EditEmail' && (
+                                            <>
+                                                <h2 className={style.sheader}>Edit Email</h2>
+                                                <div className={style.sbody}>
+                                                    <div className={style.sbodyElement}>
+                                                        <div className={style.sbodyElement}>
+                                                            Mail
+                                                            <input></input>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )}
+                                        {settingTab === 'DeleteProfile' && (
+                                            <>
+                                                <h2 className={style.sheader}>Delete Profile</h2>
+                                                <hr/>
+                                                <div className={style.sbody}>
+                                                    <p>Before confirming that you would like your profile deleted, we'd like to take a moment to explain the implications of deletion:</p>
 
+                                                    <p>Deletion is irreversible, and you will have no way to regain any of your original content, should this deletion be carried out and you change your mind later on.</p>
+                                                    <p>Your questions and answers will remain on the site, but will be disassociated and anonymized (the author will be listed as "user23264375") and will not indicate your authorship even if you later return to the site.</p>
+                                                
+                                                    <p>Confirming deletion will only delete your profile on Stack Overflow - it will not affect any of your other profiles on the Stack Exchange network. If you want to delete multiple profiles, you'll need to visit each site separately and request deletion of those individual profiles.</p>
+
+                                                    <label>
+                                                            <input type="checkbox" required onChange={() => setIsConfirmed(!isConfirmed)} />
+
+                                                            I have read the information stated above and understand the implications of having my profile deleted. I wish to proceed with the deletion of my profile.
+                                                    </label>
+                                                    
+                                                    <button className={style.deleteaccount} onClick={handleDeleteProfile}>Delete Profile</button>
+                                                </div>
+                                            </>
+                                        )}
                                     </div>
                                 </>
                             )}
